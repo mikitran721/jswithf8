1cbe1c69c2685828c53f0e686cdce5603d603086
MikiTran <mikitran721@gmail.com>
2023-07-26 12:35:30 +0700

    validate form1 ok

--- js/loginAuth.js
+++ js/loginAuth.js
@@ -1,11 +1,20 @@
 /* ham nay de tai su dung lai Auth */
 // var $ = document.querySelector;
 function Validator(options) {
+  var selectorRules = {};
   //   tach ham xu ly validate
   const validate = (inputElement, rule) => {
-    // lay value
     // goi ham test
-    var errorMessage = rule.test(inputElement.value);
+    // var errorMessage = rule.test(inputElement.value);
+    var errorMessage = "";
+
+    let rules = selectorRules[rule.selector];
+
+    for (let i = 0; i < rules.length; i++) {
+      errorMessage = rules[i](inputElement.value);
+      if (errorMessage) break;
+    }
+
     // console.log(errorMessage);
     //   lay the span bao loi di kem item
     var messageElement = inputElement.parentElement.querySelector(
@@ -19,6 +28,8 @@ function Validator(options) {
       messageElement.innerText = "";
       inputElement.parentElement.classList.remove("invalid");
     }
+
+    return !!errorMessage;
   };
 
   /* function: bo thong bao loi */
@@ -34,8 +45,45 @@ function Validator(options) {
   let formElement = document.querySelector(options.form);
 
   if (formElement) {
+    formElement.onsubmit = (e) => {
+      e.preventDefault();
+      let isFormValid = true;
+
+      // lap qua tung rule va validate luon
+      options.rules.forEach((rule) => {
+        let inputElement = document.querySelector(rule.selector);
+        let isInValid = validate(inputElement, rule);
+        if (isInValid) {
+          isFormValid = false;
+        }
+      });
+
+      let enableInputs = formElement.querySelectorAll("[name]:not([disabled])");
+
+      if (isFormValid) {
+        let formValues = Array.from(enableInputs).reduce((values, input) => {
+          return (values[input.name] = input.value) && values;
+        }, {});
+
+        if (typeof options.onSubmit === "function") {
+          // submit voi JS
+          options.onSubmit(formValues);
+        } else {
+          //khi nay dev van sd method submit cua form
+          formElement.submit();
+        }
+      }
+    };
     // lay cac selector dc truyen vao theo options.rules
     options.rules.forEach((rule) => {
+      // luu selectorRule cho moi element;
+      if (Array.isArray(selectorRules[rule.selector])) {
+        selectorRules[rule.selector].push(rule.test);
+      } else {
+        selectorRules[rule.selector] = [rule.test];
+      }
+      // selectorRules[rule.selector] = rule.test;
+
       // lay ra cac element
       var inputElement = document.querySelector(rule.selector);
 
@@ -58,31 +106,47 @@ function Validator(options) {
     1. khi co loi => tra ra message loi
     2. Khi hop le => ko tra gi ca (undefined)
 */
-Validator.isRequired = function (selector) {
+Validator.isRequired = function (selector, errorMessage) {
   return {
     selector,
     test(value) {
-      return value.trim() ? undefined : "Vui long nhap truong nay";
+      return value.trim()
+        ? undefined
+        : errorMessage || "Vui long nhap truong nay";
     },
   };
 };
 
-Validator.isEmail = function (selector) {
+Validator.isEmail = function (selector, errorMessage) {
   return {
     selector,
     test(value) {
       var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-      return regex.test(value) ? undefined : "Vui long nhap email";
+      return regex.test(value)
+        ? undefined
+        : errorMessage || "Vui long nhap email";
     },
   };
 };
-Validator.minLength = function (selector, min) {
+Validator.minLength = function (selector, min, errorMessage) {
   return {
     selector,
     test(value) {
       return value.trim().length >= min
         ? undefined
-        : `Vui long nhap toi thieu ${min} ky tu`;
+        : errorMessage || `Vui long nhap toi thieu ${min} ky tu`;
+    },
+  };
+};
+
+// check repassword
+Validator.isConfirmed = (selector, getConfirmValue, errorMessage) => {
+  return {
+    selector,
+    test(value) {
+      return value.trim() === getConfirmValue()
+        ? undefined
+        : errorMessage || "Gia tri nhap vao chua dung";
     },
   };
 };
